e1183191d28515ecbcea46ab792bfc0e00e47315
Ramon <ramonhollands@gmail.com>
2018-11-07 20:30:21 +0100

    Prettify layout and switched to Flask

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,15 @@ FROM python:3.6-slim-stretch
 RUN apt update
 RUN apt install -y python3-dev gcc
 
-# Install pytorch and fastai
+# Install pytorch, fastai and Flask
 RUN pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 RUN pip install fastai
-
-# Install starlette and uvicorn
-RUN pip install starlette uvicorn python-multipart aiohttp
+RUN pip install Flask
 
 ADD models/3_resnet34_defaults.pth models/3_resnet34_defaults.pth
 ADD predict.py predict.py
+ADD static static
+ADD templates templates
 
 # Run it once to trigger resnet download
 RUN python predict.py
@@ -19,4 +19,7 @@ RUN python predict.py
 EXPOSE 8008
 
 # Start the server
-CMD ["python", "predict.py", "serve"]
+CMD ["python", "predict.py"]
+
+# export FLASK_APP=predict.py
+# flask run --host=0.0.0.0
--- predict.py
+++ predict.py
@@ -1,15 +1,11 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse
 from fastai import *
 from fastai.vision import *
 import torch
 from pathlib import Path
 
-import uvicorn
-import aiohttp
-import asyncio
-
+from flask import Flask, render_template, request
 from io import BytesIO
+import os
 
 # path = Path('/Users/ramon/AI/mushrooms')
 path = Path(".")
@@ -22,45 +18,47 @@ classes = keywords.split(',')
 
 data = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=224).normalize(imagenet_stats)
 learn = create_cnn(data, models.resnet34, metrics=accuracy)
-learn.model.eval();
+learn.model.eval()
 learn.load('3_resnet34_defaults')
 
-# img = open_image(path/'images'/'eekhoorntjesbrood.jpg')
-# results = learn.predict(img)
-# print(results)
+# img = open_image(path/'images'/'gewoon eekhoorntjesbrood'/'eekhoorntjesbrood.jpg')
+# (class_name, nr_of_classes, losses) = learn.predict(img)
 
-app = Starlette()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('upload.html')
 
 @app.route("/upload", methods=["POST"])
-async def upload(request):
-    data = await request.form()
-    bytes = await (data["file"].read())
+def upload():
+    bytes = request.files['file'].read()
     return predict_image_from_bytes(bytes)
 
-@app.route("/")
-def form(request):
-    return HTMLResponse(
-        """
-        <form action="/upload" method="post" enctype="multipart/form-data">
-            Select image to upload:
-            <input type="file" name="file">
-            <input type="submit" value="Upload Image">
-        </form>
-        Or submit a URL:
-        <form action="/classify-url" method="get">
-            <input type="url" name="url">
-            <input type="submit" value="Fetch and analyze image">
-        </form>
-    """)
+# @app.route("/")
+# def form(request):
+#     env = Environment(
+#         loader=PackageLoader('static', 'templates'),
+#         autoescape=select_autoescape(['html', 'xml'])
+#     )
+#     template = env.get_template('upload.html')
+#     return HTMLResponse(template.render())
 
 def predict_image_from_bytes(bytes):
     img = open_image(BytesIO(bytes))
     (class_name, nr_of_classes, losses) = learn.predict(img)
-    return JSONResponse({
-        "prediction": class_name,
-    })
+    example_image = os.listdir(path/'static'/'images'/class_name)[0]
+    # strings = ["%.2f" % loss for loss in losses.numpy()]
+    return 'Looks like the "'+class_name+'":<br /><br /><img src="/static/images/'+class_name+'/'+example_image+'" />'
 
-if __name__ == "__main__":
-    if "serve" in sys.argv:
-        port = int(os.environ.get("PORT", 8008))
-        uvicorn.run(app, host="0.0.0.0", port=port)
+# def get_view_predictions(class_name, losses):
+#     return HTMLResponse(class_name + '<br/>' + '<br />'.join(losses))
+#
+# if __name__ == "__main__":
+#     if "serve" in sys.argv:
+#         port = int(os.environ.get("PORT", 8008))
+#         uvicorn.run(app, host="0.0.0.0", port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8008))
+    app.run(debug=True, host='0.0.0.0', port=port)
